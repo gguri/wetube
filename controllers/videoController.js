b35ea5758837e93cfc74f8c82c453f423e70ab07
jc <wjdcjf0219@gmail.com>
2020-03-28 17:04:35 +0900

    200328

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,6 +1,21 @@
-export const home = (req, res) => res.render("home", { pageTitle : "Home"})
-export const search = (req, res) => res.render("search", { pageTitle : "Search"})
-export const videos = (req, res) => res.render("videos", { pageTitle : "Videos"})
+import {videos} from "../db"
+
+export const home = (req, res) => {
+    res.render("home", { pageTitle : "Home", videos});
+};
+
+export const search = (req, res) => {
+    // const searcingBy = req.query.term; 바닐라문법
+    const {
+        query: { term: searcingBy }
+
+    } = req; // es6문법
+    res.render("search", { 
+        pageTitle : "Search"
+        , searcingBy});
+};
+// searcingBy = searcingBy babel 이 있다면 searcingBy만 써도 무방
+    
 export const upload = (req, res) => res.render("upload", { pageTitle : "Upload"}) 
 export const videoDetail = (req, res) => res.render("videoDetail", { pageTitle : "Video Detail"}) 
 export const editVideo = (req, res) => res.render("editVideo", { pageTitle : "Edit Video"})
